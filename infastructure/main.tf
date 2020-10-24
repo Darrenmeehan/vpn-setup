7d32f2c878f765dbd5af75a2c0f6da9534076ae7
Darren Meehan <hi@drn.ie>
2020-10-24 17:05:29 +0100

    Adding new EC2

--- infastructure/main.tf
+++ infastructure/main.tf
@@ -4,3 +4,34 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
+
+resource "aws_eip" "public_ip" {
+  instance = aws_instance.vpn.id
+  vpc      = true
+}
+
+output "public_ip" {
+  value = aws_eip.public_ip.public_ip
+}
+
+data "aws_ami" "amazonlinux" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+resource "aws_instance" "vpn" {
+  ami           = data.aws_ami.amazonlinux.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+output "ec2_instance_id" {
+  value = aws_instance.vpn.id
+}
