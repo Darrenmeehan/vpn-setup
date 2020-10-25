5c71bf67a6155f386b73bb0ce789a1cce5da5cf4
Darren Meehan <hi@drn.ie>
2020-10-25 10:47:32 +0000

    Moving basic vpc into networking module

--- infastructure/main.tf
+++ infastructure/main.tf
@@ -1,17 +1,5 @@
-# Networking
-
-# Managing default VPC
-# These usually allow all traffic, will be setitng this up to restrict to no usuage.
-
-resource "aws_default_vpc" "default" {
-  tags = {
-    Name = "Default VPC"
-  }
-}
-
-# Create a VPC
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+module "networking" {
+    source = "./modules/networking"
 }
 
 module "server" {
